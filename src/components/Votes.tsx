8636a503d45a9e42eb00dc13a74967cd3d94c2df
Leo <leonardogilli@outlook.com>
2023-02-27 23:46:39 -0300

    Flip cards

--- src/components/Votes.tsx
+++ src/components/Votes.tsx
@@ -22,45 +22,75 @@ const Votes: React.FC<Votes> = ({ users, reveal }) => {
         userSelect: 'none',
         flexDirection: 'row',
         flexWrap: 'wrap',
-        width: users && users.length * 70,
-        columnGap: 5,
+        width: users && users.length * 55,
+        columnGap: 7,
         rowGap: matchesMd ? 21 : 22
       }}>
       {users?.length &&
         users.map(user => (
           <div key={user.clientId}>
             <Box
+              id='flip-card'
               sx={{
+                backgroundColor: 'transparent',
+                height: matchesMd ? 77 : 90,
+                width: matchesMd ? 46 : 54,
+                perspective: '1000px',
                 color: '#3993ff',
                 fontSize: matchesMd ? 21 : 22,
                 fontWeight: 700,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: matchesMd ? 77 : 90,
-                width: matchesMd ? 45 : 52,
-                border:
-                  user.card && !reveal
-                    ? '1px solid #ededed25'
-                    : user.card && reveal
-                    ? '2px solid #3993ff'
-                    : '1px solid transparent',
                 borderRadius: '10px',
-                backgroundColor: user.card
-                  ? '#fff'
-                  : user.card && reveal
-                  ? '#fff'
-                  : '#e8e9ea',
-                marginX: 0.5,
-                marginY: 0.3,
-                backgroundImage: user.card && !reveal ? 'url(/logo.png)' : '',
-                boxShadow: user.card && !reveal ? '2px 2px 5px #00000095' : '',
-                backgroundRepeat: 'no-repeat',
-                backgroundPositionX: 'center',
-                backgroundPositionY: 'center',
-                backgroundSize: 40
+                marginBottom: 0.3
               }}>
-              {reveal ? user.card : ''}
+              <Box
+                id='flip-card-inner'
+                sx={{
+                  position: 'relative',
+                  width: '100%',
+                  height: '100%',
+                  textAlign: 'center',
+                  transition: 'transform 0.6s',
+                  transformStyle: 'preserve-3d',
+                  transform: user.card && reveal ? 'rotateY(180deg)' : '',
+                  borderRadius: '10px'
+                }}>
+                <Box
+                  id='back'
+                  sx={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    webkitBackfaceVisibility: 'hidden' /* Safari */,
+                    backfaceVisibility: 'hidden',
+                    background: user.card ? 'url(/logo.png)' : '#e8e9ea',
+                    boxShadow:
+                      user.card && !reveal ? '2px 2px 5px #00000095' : '',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPositionX: 'center',
+                    backgroundPositionY: 'center',
+                    backgroundSize: 40,
+                    borderRadius: '10px',
+                    transition: reveal ? 'box-shadow .3s' : ''
+                  }}></Box>
+                <Box
+                  id='front'
+                  sx={{
+                    position: 'absolute',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    border: '2px solid #3993ff',
+                    width: '100%',
+                    height: '100%',
+                    webkitBackfaceVisibility: 'hidden' /* Safari */,
+                    backfaceVisibility: 'hidden',
+                    backgroundColor: '#fff',
+                    transform: 'rotateY(180deg)',
+                    borderRadius: '10px'
+                  }}>
+                  {reveal ? user.card : ''}
+                </Box>
+              </Box>
             </Box>
 
             <Typography
